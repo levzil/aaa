bc272db92d7465e2c35f627279ce63cb31f72db3
levzil <lev-zilberman@hotmail.com>
2021-09-02 15:36:47 -0400

    First load from file on my pc to my test github repository in a cloud
    First load from file on my pc to my test github repository in a cloud

---
+++ aaa/Assets/firstScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // comment 1
+        // comment 2
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
